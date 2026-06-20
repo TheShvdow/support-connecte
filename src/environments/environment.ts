4d53b90566d5b6dbd53aeefbc358e5fd5bee6386
TheShvdow <derisswade@gmail.com>
2026-06-20 06:33:28 +0000

    fix: mettre à jour la clé Supabase dans les fichiers d'environnement

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   supabaseUrl: 'https://eahzgcnwafyxkuzqzxnr.supabase.co',
-  supabaseKey: 'sb_publishable_-3Io8xTnK2YztWZ_4bMwvw_XBiuTfVG',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVhaHpnY253YWZ5eGt1enF6eG5yIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE5MTY1NzUsImV4cCI6MjA5NzQ5MjU3NX0.XIqAT3rChfKb3RduETiWukafzIk-icVGeBF8E82EmTY',
 };
